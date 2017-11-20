2d518b2f4a6aadaec791a07ba189f4adc0960360
dearth <Dearth>
2017-11-20 00:07:23 -0700

    Started work on Population class

--- Population.java
+++ Population.java
@@ -1,7 +1,56 @@
 package PopGenetics;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Population implements Serializable{
+	protected int generation;
+	protected double mutation_rate;
+	protected double selection_strength;
+	protected Individual population[];
+	protected Random random;
+	
+	public Population(int pop_size, double mut_rate, double s_str) {
+		generation = 0;
+		population = new Individual[pop_size];
+		random = new Random();
+		mutation_rate = mut_rate;
+		selection_strength = s_str;
+	}
+	
+	public Population() {
+		this(100, 0.1, 1.0);
+	}
+	
+	public void sortPopulation() {
+		Arrays.sort(this.population);
+	}
+	
+	public void setGeneration(int g) {
+		this.generation = g;
+	}
+	
+	public void setMutationRate(double r) {
+		this.mutation_rate = r;
+	}
+	
+	public void setSelectionStrength(double s) {
+		this.selection_strength = s;
+	}
+	
+	public void mutatePopulation() {
+		for (int i = 0; i < this.population.length; i++) {
+			if(random.nextDouble() < this.mutation_rate) {
+				if(random.nextBoolean()) {
+					this.population[i].mutate_x_genome();
+				} else {
+					this.population[i].mutate_y_genome();
+				}
+			}
+		}
+	}
+	
+	public void 
 	
 }
